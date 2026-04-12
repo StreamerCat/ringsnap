93c1c654ca4b594e918acd59d379825231969480
StreamerCat <168151900+StreamerCat@users.noreply.github.com>
2026-04-11 22:33:22 -0600

    Standardize PostHog host and shutdown in create-trial

--- supabase/functions/_shared/posthog.ts
+++ supabase/functions/_shared/posthog.ts
@@ -1,11 +1,10 @@
 import { PostHog } from "npm:posthog-node";
 
 const posthogApiKey = Deno.env.get("POSTHOG_API_KEY");
-const posthogHost = Deno.env.get("POSTHOG_HOST") ?? "https://us.i.posthog.com";
 
 export const posthog = posthogApiKey
   ? new PostHog(posthogApiKey, {
-    host: posthogHost,
+    host: "https://us.i.posthog.com",
     flushAt: 1,
     flushInterval: 0,
   })
